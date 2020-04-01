594a1116a6d094b9f6a871d286afcc18a7ce1a58
ondrej11 <ondrej.carnogursky@gmail.com>
2020-04-01 08:31:29 +0200

    Add files via upload

---
+++ precache-manifest.4c3106e1a7c635b8a6715dc59bbf0798.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0674b43f856956f673b910c6acf01ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6abf7e8202654897ae92",
+    "url": "/static/css/2.da92d59b.chunk.css"
+  },
+  {
+    "revision": "86d16f38f129b0bd8120",
+    "url": "/static/css/main.b9364cda.chunk.css"
+  },
+  {
+    "revision": "6abf7e8202654897ae92",
+    "url": "/static/js/2.fb24f133.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/static/js/2.fb24f133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86d16f38f129b0bd8120",
+    "url": "/static/js/main.b2650f9b.chunk.js"
+  },
+  {
+    "revision": "18770af5b8fe3af4dde2",
+    "url": "/static/js/runtime-main.84259ce8.js"
+  }
+]);
