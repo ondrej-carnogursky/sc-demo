a063bdf020cccdcad67a9124966debff7ef05985
Ondrej Carnogursky <ondrej.carnogursky@gmail.com>
2020-04-01 10:34:29 +0200

    fix homepage to ./sc-demo

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bae82c7052b9ce0fb57861d9cdbb74c4.js"
+  "./sc-demo/precache-manifest.6a5356ba24fbb88c0e37b1e7fd6f1951.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./sc-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
